827526b3f0bc15a72e11012038a5032380518137
anselbrandt <ansel@anselbrandt.com>
2020-11-05 21:48:12 -0500

    GraphQL server working

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
+import "reflect-metadata";
 import express from "express";
 import http from "http";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
 
 const PORT = process.env.PORT || 4000;
 
@@ -9,6 +13,21 @@ const main = async () => {
   app.get("/", (_, res) => {
     res.send("API server up and running.");
   });
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+    context: ({ req, res }) => {
+      return {
+        req,
+        res,
+      };
+    },
+  });
+
+  apolloServer.applyMiddleware({ app, cors: false });
 
   const httpServer = http.createServer(app);
 
