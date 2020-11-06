05b32f2633af452ca4d792870dd63f567aa98cfe
anselbrandt <ansel@anselbrandt.com>
2020-11-05 23:20:25 -0500

    GraphQL subscriptions working

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,29 @@
 import "reflect-metadata";
 import express from "express";
 import http from "http";
+import Redis from "ioredis";
+import { RedisPubSub } from "graphql-redis-subscriptions";
 import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
 import { HelloResolver } from "./resolvers/hello";
 
 const PORT = process.env.PORT || 4000;
+const REDIS_URL = process.env.REDIS_URL || `127.0.0.1:6379`;
 
 const main = async () => {
+  const redis = new Redis(REDIS_URL);
+
+  const pubsub = new RedisPubSub({
+    publisher: new Redis(REDIS_URL),
+    subscriber: new Redis(REDIS_URL),
+  });
+
   const app = express();
+
+  app.use((req: any, _, next: any) => {
+    req.pubsub = pubsub;
+    next();
+  });
 
   app.get("/", (_, res) => {
     res.send("API server up and running.");
@@ -17,19 +32,38 @@ const main = async () => {
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
       resolvers: [HelloResolver],
+      pubSub: pubsub,
       validate: false,
     }),
     context: ({ req, res }) => {
       return {
         req,
         res,
+        redis,
+        pubsub,
       };
+    },
+    subscriptions: {
+      async onConnect(_, webSocket: any) {
+        console.log(
+          "connected: ",
+          webSocket.upgradeReq.headers["sec-websocket-key"]
+        );
+      },
+      async onDisconnect(webSocket: any) {
+        console.log(
+          "disconnected: ",
+          webSocket.upgradeReq.headers["sec-websocket-key"]
+        );
+      },
     },
   });
 
   apolloServer.applyMiddleware({ app, cors: false });
 
   const httpServer = http.createServer(app);
+
+  apolloServer.installSubscriptionHandlers(httpServer);
 
   httpServer.listen(PORT, () => {
     console.log(`server started on localhost:${PORT}`);
--- src/resolvers/hello.ts
+++ src/resolvers/hello.ts
@@ -1,9 +1,17 @@
-import { Resolver, Query } from "type-graphql";
+import { Ctx, Resolver, Query, Subscription } from "type-graphql";
 
 @Resolver()
 export class HelloResolver {
   @Query(() => String)
-  async hello() {
-    return "GraphQL server up and running.";
+  async hello(@Ctx() ctx: any) {
+    await ctx.req.pubsub.publish("MESSAGES");
+    return "hello";
+  }
+
+  @Subscription(() => String, {
+    topics: "MESSAGES",
+  })
+  async subscription(): Promise<any> {
+    return "User query";
   }
 }
